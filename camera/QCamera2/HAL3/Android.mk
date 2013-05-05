e74d4777bf965e774455c9c045152c9d827e2442
Shuzhen Wang <shuzhenw@codeaurora.org>
2013-05-04 17:33:01 -0700

    Camera3: Code clean up and bug fix
    - Stop all streams before removing any of them. This is due to
      hardware limitation.
    - Remove compilation warnings.
    - Eliminate log flooding.
    - Remove conditional compiled-out code.
    
    Conflicts:
    
    	QCamera2/stack/mm-camera-interface/src/mm_camera_stream.c
    
    Conflicts:
    
    	QCamera2/stack/mm-camera-interface/src/mm_camera_stream.c
    
    Change-Id: Ice1a9644b7d40130e377d1d4d5c713ff52f7d473

--- camera/QCamera2/HAL3/Android.mk
+++ camera/QCamera2/HAL3/Android.mk
@@ -13,9 +13,8 @@ LOCAL_SRC_FILES := \
         ../util/QCameraCmdThread.cpp \
         ../util/QCameraQueue.cpp
 
-#LOCAL_CFLAGS = -Wall -Werror
+LOCAL_CFLAGS = -Wall -Werror
 LOCAL_CFLAGS := -Wall
-LOCAL_CFLAGS += -DFAKE_FRAME_NUMBERS
 
 
 LOCAL_C_INCLUDES := \
