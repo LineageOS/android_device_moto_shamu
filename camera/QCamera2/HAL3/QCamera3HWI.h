42e02b20b279deb07dd6fc186535dcf9cf1df55d
Shuzhen Wang <shuzhenw@codeaurora.org>
2013-09-25 23:43:50 -0700

    Camera3: Add frame drop info to metadata
    Add CAM_INTF_META_FRAME_DROPPED to meta data to know whether
    any stream buffer is dropped or not. If dropped, send error
    notify to framework with CAMERA3_MSG_ERROR_BUFFER and
    corresponding buffer status with CAMERA3_BUFFER_STATUS_ERROR.
    
    Bug: 10844261
    Change-Id: I1b7e38790c8314fce8895b3ccc51c79b748e7a32

--- camera/QCamera2/HAL3/QCamera3HWI.h
+++ camera/QCamera2/HAL3/QCamera3HWI.h
@@ -207,6 +207,10 @@ private:
         jpeg_settings_t input_jpeg_settings;
         int input_buffer_present;
     } PendingRequestInfo;
+    typedef struct {
+        uint32_t frame_number;
+        uint32_t stream_type_mask;;
+    } PendingFrameDropInfo;
     typedef KeyedVector<camera3_stream_t *, uint32_t> PendingBuffersMap;
     /*Data structure to store metadata information*/
     typedef struct {
@@ -217,6 +221,7 @@ private:
 
     List<MetadataBufferInfo> mStoredMetadataList;
     List<PendingRequestInfo> mPendingRequestsList;
+    List<PendingFrameDropInfo> mPendingFrameDropList;
     PendingBuffersMap mPendingBuffersMap;
     pthread_cond_t mRequestCond;
     int mPendingRequest;
