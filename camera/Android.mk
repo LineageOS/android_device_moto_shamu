6aa43bc88dd0857c66257cad6082769d0cadf706
Abhishek Airan <aairan@codeaurora.org>
2013-11-19 17:38:26 +0530

    camera: Include mpq8092 for compiling libomx_core.so
    -  Add mpq8092 to the list of drivers to remove compilation error
    
    Change-Id: Ifeae44e4cb0982857a85053f8052b2b10325d362

--- camera/Android.mk
+++ camera/Android.mk
@@ -3,6 +3,7 @@ MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8226
 MM_V4L2_DRIVER_LIST += msm8610
 MM_V4L2_DRIVER_LIST += apq8084
+MM_V4L2_DRIVER_LIST += mpq8092
 
 ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
   ifneq ($(USE_CAMERA_STUB),true)
