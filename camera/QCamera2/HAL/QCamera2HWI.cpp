2d5f09e3151a50c666439784a466e03535857880
Rajaram Gudivada <rajaramg@codeaurora.org>
2014-01-15 18:01:51 +0530

    Camera HAL1.0: Buffer Optimization for single ZSL snapshot
    ZSL snapshot buffers has reduced from 8 to 5.
    i.e, Unmatched count is removed and 5 buffers
    enough for ZSL streaming
    
    Before this change:
    ZSL Snapshot Buffers = 8
    
    With this change:
    ZSL Snapshot Buffers = 5
    
    5 ZSL snapshot buffers break up:
    2 for HAL ZSL matched Queue
    2 for ISP Ping Ping Pong Logic
    1 for maintaining the streaming
    
    Change-Id: Ie46549567c9c08f08830894bd9cd1c37d9734a5a

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundataion. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -1356,7 +1356,14 @@ uint8_t QCamera2HardwareInterface::getBufNumRequired(cam_stream_type_t stream_ty
     case CAM_STREAM_TYPE_SNAPSHOT:
         {
             if (mParameters.isZSLMode() || mLongshotEnabled) {
-                bufferCnt = zslQBuffers + minCircularBufNum;
+                if (minCaptureBuffers == 1 && !mLongshotEnabled) {
+                    // Single ZSL snapshot case
+                    bufferCnt = zslQBuffers + CAMERA_MIN_STREAMING_BUFFERS;
+                }
+                else {
+                    // ZSL Burst or Longshot case
+                    bufferCnt = zslQBuffers + minCircularBufNum;
+                }
             } else {
                 bufferCnt = minCaptureBuffers*CAMERA_PPROC_OUT_BUFFER_MULTIPLIER +
                             mParameters.getNumOfExtraHDRInBufsIfNeeded() -
