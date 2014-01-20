8b1f07bbb6fac19ef2f2854fd29764d0270d9aa5
Vijay kumar Tumati <vtumati@codeaurora.org>
2014-01-20 12:15:50 +0530

    Camera: Send snapshot frame for reprocess if crop feature is enabled
    If none of the other PP features are
    enabled, Snapshot frames are not being
    sent to reprocess and outpur jpeg
    doesn't have zoom. This will fix that.
    
    Change-Id: Ie747bf714b2e9509361cf29e6b475a5da7f5f4a2

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundataion. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -3928,6 +3928,10 @@ QCameraReprocessChannel *QCamera2HardwareInterface::addOnlineReprocChannel(
         if (gCamCapability[mCameraId]->min_required_pp_mask & CAM_QCOM_FEATURE_SHARPNESS) {
             pp_config.feature_mask |= CAM_QCOM_FEATURE_SHARPNESS;
             pp_config.sharpness = mParameters.getInt(QCameraParameters::KEY_QC_SHARPNESS);
+        }
+
+        if (gCamCapability[mCameraId]->min_required_pp_mask & CAM_QCOM_FEATURE_CROP) {
+            pp_config.feature_mask |= CAM_QCOM_FEATURE_CROP;
         }
 
         if (mParameters.isWNREnabled()) {
