c870ea65cf829c5d2e01805c4ffd8ff0ab2577d9
Emilian Peev <epeevs@codeaurora.org>
2014-02-10 16:09:48 +0200

    Camera3: Permit larger metadata buffers
     Metadata buffers could be padded with
     plane padding. This patch would allow
     for larger buffer to be used.
    
    Change-Id: Ife308783c6141ec842531c336fd70c593d238496

--- camera/QCamera2/HAL3/QCamera3Channel.cpp
+++ camera/QCamera2/HAL3/QCamera3Channel.cpp
@@ -808,9 +808,11 @@ void QCamera3MetadataChannel::streamCbRoutine(
 QCamera3Memory* QCamera3MetadataChannel::getStreamBufs(uint32_t len)
 {
     int rc;
-    if (len != sizeof(metadata_buffer_t)) {
-        ALOGE("%s: size doesn't match %d vs %d", __func__,
-                len, sizeof(metadata_buffer_t));
+    if (len < sizeof(metadata_buffer_t)) {
+        ALOGE("%s: Metadata buffer size less than structure %d vs %d",
+                __func__,
+                len,
+                sizeof(metadata_buffer_t));
         return NULL;
     }
     mMemory = new QCamera3HeapMemory();
