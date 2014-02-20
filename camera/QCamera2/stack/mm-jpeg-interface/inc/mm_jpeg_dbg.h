514deb26b50744f98abf4e832564aea26d60cf9f
Ranjith Kagathi Ananda <ranjith@codeaurora.org>
2014-02-19 20:13:38 -0800

    QCamera2/stack/mm-jpeg-interface: Enable/disable debug logs runtime
    - adds the property "persist.camera.logs". This chooses debug
    log level and this will not affect the error logs
    - 0: turns off CDBG and CDBG_HIGH logs
    - 1: turns-on CDBG_HIGH logs
    - 2: turns-on CDBG_HIGH and CDBG logs
    
    Change-Id: Ia1e282050559deaea3d32d913e0e0623b0f96e8e

--- camera/QCamera2/stack/mm-jpeg-interface/inc/mm_jpeg_dbg.h
+++ camera/QCamera2/stack/mm-jpeg-interface/inc/mm_jpeg_dbg.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -30,6 +30,13 @@
 #ifndef __MM_JPEG_DBG_H__
 #define __MM_JPEG_DBG_H__
 
+#define LOG_DEBUG 1
+/* Choose debug log level. This will not affect the error logs
+   0: turns off CDBG and CDBG_HIGH logs
+   1: turns-on CDBG_HIGH logs
+   2: turns-on CDBG_HIGH and CDBG logs */
+extern volatile uint32_t gMmJpegIntfLogLevel;
+
 #ifndef LOG_DEBUG
   #ifdef _ANDROID_
     #undef LOG_NIDEBUG
@@ -50,7 +57,7 @@
     #define LOG_NIDEBUG 0
     #define LOG_TAG "mm-jpeg-intf"
     #include <utils/Log.h>
-    #define CDBG(fmt, args...) ALOGE(fmt, ##args)
+    #define CDBG(fmt, args...) ALOGD_IF(gMmJpegIntfLogLevel >= 2, fmt, ##args)
   #else
     #include <stdio.h>
     #define CDBG(fmt, args...) fprintf(stderr, fmt, ##args)
@@ -59,7 +66,7 @@
 #endif
 
 #ifdef _ANDROID_
-  #define CDBG_HIGH(fmt, args...)  ALOGE(fmt, ##args)
+  #define CDBG_HIGH(fmt, args...) ALOGD_IF(gMmJpegIntfLogLevel >= 1, fmt, ##args)
   #define CDBG_ERROR(fmt, args...)  ALOGE(fmt, ##args)
 #else
   #define CDBG_HIGH(fmt, args...) fprintf(stderr, fmt, ##args)
--- camera/QCamera2/stack/mm-jpeg-interface/src/mm_jpeg_interface.c
+++ camera/QCamera2/stack/mm-jpeg-interface/src/mm_jpeg_interface.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -33,6 +33,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <cutils/properties.h>
 
 #include "mm_jpeg_dbg.h"
 #include "mm_jpeg_interface.h"
@@ -43,6 +44,7 @@ static mm_jpeg_obj* g_jpeg_obj = NULL;
 
 static pthread_mutex_t g_handler_lock = PTHREAD_MUTEX_INITIALIZER;
 static uint16_t g_handler_history_count = 0; /* history count for handler */
+volatile uint32_t gMmJpegIntfLogLevel = 0;
 
 /** mm_jpeg_util_generate_handler:
  *
@@ -290,6 +292,10 @@ uint32_t jpeg_open(mm_jpeg_ops_t *ops, mm_dimension picture_size)
   int32_t rc = 0;
   uint32_t clnt_hdl = 0;
   mm_jpeg_obj* jpeg_obj = NULL;
+  char prop[PROPERTY_VALUE_MAX];
+
+  property_get("persist.camera.logs", prop, "0");
+  gMmJpegIntfLogLevel = atoi(prop);
 
   pthread_mutex_lock(&g_intf_lock);
   /* first time open */
