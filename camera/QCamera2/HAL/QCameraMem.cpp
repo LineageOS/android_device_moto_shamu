RonGokhale/android_hardware_qcom_camera

QCamera: Fix for AEC fluctuation in video recording.
Flag to indicate color transform to video encoder was not initialized.
Added 709 flag by default. Video frames/recorded video will be of 709.

Change-Id: I023212b0339a5b56f65e5db361a2b70d4775a471

QCamera2/HAL/QCameraMem.cpp
@@ -1115,9 +1115,11 @@ int QCameraVideoMemory::allocate(int count, int size, uint32_t isSecure)
         packet->meta_handle = native_handle_create(1, 2); //1 fd, 1 offset and 1 size
         packet->buffer_type = kMetadataBufferTypeCameraSource;
         native_handle_t * nh = const_cast<native_handle_t *>(packet->meta_handle);
+        memset(nh, 0x0, sizeof(nh));
         nh->data[0] = mMemInfo[i].fd;
         nh->data[1] = 0;
         nh->data[2] = mMemInfo[i].size;
+        nh->data[3] = private_handle_t::PRIV_FLAGS_ITU_R_709;
     }
     mBufferCount = count;
     return NO_ERROR;

LineageOS/android_device_moto_shamu

camera/QCamera2/HAL/QCameraMem.cpp
@@ -1115,9 +1115,11 @@ int QCameraVideoMemory::allocate(int count, int size, uint32_t isSecure)
         packet->meta_handle = native_handle_create(1, 2); //1 fd, 1 offset and 1 size
         packet->buffer_type = kMetadataBufferTypeCameraSource;
         native_handle_t * nh = const_cast<native_handle_t *>(packet->meta_handle);
+        memset(nh, 0x0, sizeof(native_handle_t));
         nh->data[0] = mMemInfo[i].fd;
         nh->data[1] = 0;
         nh->data[2] = mMemInfo[i].size;
+        nh->data[3] = private_handle_t::PRIV_FLAGS_ITU_R_709;
     }
     mBufferCount = count;
     return NO_ERROR;