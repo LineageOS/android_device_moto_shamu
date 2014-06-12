22e39a1ab85e54ab36e1ac7420209a292fbe7059
bshao1 <bshao1@motorola.com>
2014-06-12 10:10:08 -0500

    Bringup BCM20795 NFC on shamu.
    Change-Id: I6760d9935c946dbd0079824db1ffa147aa5aa79d

--- device.mk
+++ device.mk
@@ -222,6 +222,15 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    device/moto/shamu/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/moto/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+
+$(call inherit-product-if-exists, vendor/broadcom/nfc/Firmware.mk)
+
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
