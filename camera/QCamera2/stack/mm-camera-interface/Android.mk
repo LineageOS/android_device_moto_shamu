e57694b2bb627a3a9c8edc542d0f3aab4bccc3a2
Shuzhen Wang <shuzhenw@codeaurora.org>
2014-06-15 16:36:06 -0700

    Camera: Support msm8084 board platform
    Some variants uses msm8084 instead of apq8084 as board platform
    name. This change addes support.
    
    Change-Id: Ie54a78a4a2a4a92d1cd10df824c489ef8ed74023

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -17,7 +17,7 @@ ifeq ($(strip $(TARGET_USES_ION)),true)
     LOCAL_CFLAGS += -DUSE_ION
 endif
 
-ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8994,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8084 msm8994,$(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DVENUS_PRESENT
 endif
 
