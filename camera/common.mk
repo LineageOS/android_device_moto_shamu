763426674134b2e8db183f7ea16b48f8e55655de
Shuzhen Wang <shuzhenw@quicinc.com>
2014-06-20 11:03:13 -0700

    QCamera2: Really remove KERNEL_OBJ dependencies

--- camera/common.mk
+++ camera/common.mk
@@ -1,7 +1,3 @@
 common_deps :=
 kernel_includes :=
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-endif
