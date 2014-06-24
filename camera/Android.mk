c89d19ca1e017a8132bb823a92013ab641b0bddb
Iliyan Malchev <malchev@google.com>
2014-06-24 12:47:06 -0700

    camera HAL: add makefile guards
    Change-Id: If57b9230f6d8c0bcc115b17a66743e880ae48d12
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8916
@@ -16,4 +18,6 @@ ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
+
 endif
