Evervolv/android_hardware_qcom_camera

Camera3: Add vendor tags for opaque raw format
Expose two static metadata tags for opaque raw capture:
stride and format. Format could be either MIPI format
or LEGACY format. Stride indicates the offset in byte
between two lines of raw data.

Bug: 14395767
Change-Id: Ic0af1b3c4e746f34636bec6a9260b9260e7ffab5

Conflicts:
	QCamera2/stack/common/cam_intf.h

QCamera2/HAL3/QCamera3VendorTags.cpp
@@ -43,7 +43,8 @@ const int QCAMERA3_SECTION_COUNT = QCAMERA3_SECTIONS_END - VENDOR_SECTION;
 
 enum qcamera3_ext_tags qcamera3_ext3_section_bounds[QCAMERA3_SECTIONS_END -
     VENDOR_SECTION] = {
-        QCAMERA3_PRIVATEDATA_END
+        QCAMERA3_PRIVATEDATA_END,
+        QCAMERA3_OPAQUE_RAW_END
 } ;
 
 typedef struct vendor_tag_info {
@@ -53,21 +54,32 @@ typedef struct vendor_tag_info {
 
 const char *qcamera3_ext_section_names[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
-    "org.codeaurora.qcamera3.privatedata"
+    "org.codeaurora.qcamera3.privatedata",
+    "org.codeaurora.qcamera3.opaque_raw"
 };
 
 vendor_tag_info_t qcamera3_privatedata[QCAMERA3_PRIVATEDATA_END - QCAMERA3_PRIVATEDATA_START] = {
-    { "privatedata", TYPE_BYTE }
+    { "privatedata_reprocess", TYPE_BYTE }
+};
+
+vendor_tag_info_t qcamera3_opaque_raw[QCAMERA3_OPAQUE_RAW_END - QCAMERA3_OPAQUE_RAW_START] = {
+    { "opaque_raw_strides", TYPE_INT32 },
+    { "opaque_raw_format", TYPE_BYTE }
 };
 
 vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
-    qcamera3_privatedata
+    qcamera3_privatedata,
+    qcamera3_opaque_raw
 };
 
 uint32_t qcamera3_all_tags[] = {
     // QCAMERA3_PRIVATEDATA
-    (uint32_t)QCAMERA3_PRIVATEDATA_REPROCESS
+    (uint32_t)QCAMERA3_PRIVATEDATA_REPROCESS,
+
+    // QCAMERA3_OPAQUE_RAW
+    (uint32_t)QCAMERA3_OPAQUE_RAW_STRIDES,
+    (uint32_t)QCAMERA3_OPAQUE_RAW_FORMAT
 };
 
 const vendor_tag_ops_t* QCamera3VendorTags::Ops = NULL;

LineageOS/android_device_moto_shamu

camera/QCamera2/HAL3/QCamera3VendorTags.cpp
@@ -44,7 +44,8 @@ const int QCAMERA3_SECTION_COUNT = QCAMERA3_SECTIONS_END - VENDOR_SECTION;
 enum qcamera3_ext_tags qcamera3_ext3_section_bounds[QCAMERA3_SECTIONS_END -
     VENDOR_SECTION] = {
         QCAMERA3_PRIVATEDATA_END,
-        QCAMERA_CDS_END
+        QCAMERA_CDS_END,
+        QCAMERA3_OPAQUE_RAW_END
 } ;
 
 typedef struct vendor_tag_info {
@@ -56,26 +57,37 @@ const char *qcamera3_ext_section_names[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
     "org.codeaurora.qcamera3.privatedata",
     "org.codeaurora.qcamera3.CDS",
+    "org.codeaurora.qcamera3.opaque_raw"
 };
 
 vendor_tag_info_t qcamera3_privatedata[QCAMERA3_PRIVATEDATA_END - QCAMERA3_PRIVATEDATA_START] = {
-    { "privatedata", TYPE_INT32 }
+    { "privatedata_reprocess", TYPE_INT32 }
 };
 
 vendor_tag_info_t qcamera3_cds[QCAMERA_CDS_END - QCAMERA3_CDS_START] = {
     { "cds", TYPE_INT32 }
 };
 
+vendor_tag_info_t qcamera3_opaque_raw[QCAMERA3_OPAQUE_RAW_END - QCAMERA3_OPAQUE_RAW_START] = {
+    { "opaque_raw_strides", TYPE_INT32 },
+    { "opaque_raw_format", TYPE_BYTE }
+};
+
 vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
     qcamera3_privatedata,
-    qcamera3_cds
+    qcamera3_cds,
+    qcamera3_opaque_raw
 };
 
 uint32_t qcamera3_all_tags[] = {
     // QCAMERA3_PRIVATEDATA
     (uint32_t)QCAMERA3_PRIVATEDATA_REPROCESS,
-    (uint32_t)QCAMERA_CDS_MODE
+    (uint32_t)QCAMERA_CDS_MODE,
+
+    // QCAMERA3_OPAQUE_RAW
+    (uint32_t)QCAMERA3_OPAQUE_RAW_STRIDES,
+    (uint32_t)QCAMERA3_OPAQUE_RAW_FORMAT
 };
 
 const vendor_tag_ops_t* QCamera3VendorTags::Ops = NULL;