8b4c00477705b3e25420c00978018d46db037730
Rick Zhang <rickz@motorola.com>
2014-07-17 14:34:49 -0700

    Set new DPI for Shamu QHD panel
    Change-Id: I8bc8f64bb3aacb5fe458732798d2554247264465

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=640
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
