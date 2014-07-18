669c346e347a06c18639738ca374970ad3835c4f
Vineeta Srivastava <vsrivastava@google.com>
2014-07-18 15:39:06 -0700

    shamu: support for xxxhdpi display
    Change-Id: Ib9e0e30ab135ad363764ef38a75fa433a7a54d20

--- device.mk
+++ device.mk
@@ -97,12 +97,12 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# This device is xhdpi.  However the platform doesn't
+# This device is xxxhdpi.  However the platform doesn't
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
