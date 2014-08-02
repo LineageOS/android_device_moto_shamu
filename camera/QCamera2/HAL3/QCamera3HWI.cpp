4dec0b297ce27cadedf7ef2f6829325945cd6da4
Seemanta Dutta <seemanta@codeaurora.org>
2014-08-01 19:26:08 -0700

    Camera3: Stop and start support stream when configuring
    When support stream is used, during configure_streams,
    support stream needs to be stopped and restarted.
    
    Change-Id: Ic483106a7fac931ba9d6e664130d59884ac69c3e

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -550,6 +550,8 @@ int QCamera3HardwareInterface::configureStreams(
         mRawDumpChannel = NULL;
     }
 
+    if (mSupportChannel)
+        mSupportChannel->stop();
     if (mMetadataChannel) {
         /* If content of mStreamInfo is not 0, there is metadata stream */
         mMetadataChannel->stop();
@@ -608,6 +610,10 @@ int QCamera3HardwareInterface::configureStreams(
     if (mMetadataChannel) {
         delete mMetadataChannel;
         mMetadataChannel = NULL;
+    }
+    if (mSupportChannel) {
+        delete mSupportChannel;
+        mSupportChannel = NULL;
     }
 
     //Create metadata channel and initialize it
