8366690533f5b9c6d10490c3bf32b6a07b037416
dhacker29 <dhackerdvm@gmail.com>
2014-12-19 00:14:59 -0500

    shamu: Allow inline kernel building
    Change-Id: I91dabcd442922e7c972c03427107d5e0ebdeb7a7

--- device.mk
+++ device.mk
@@ -20,16 +20,6 @@
 # Everything in this directory will become public
 
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/moto/shamu-kernel/zImage-dtb
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-
-PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel
-
 PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.rc:root/init.shamu.rc \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
