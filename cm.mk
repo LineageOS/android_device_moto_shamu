d671ceb332f3513f29f6b207a20fc4124063e293
dhacker29 <dhackerdvm@gmail.com>
2014-12-20 15:26:40 -0500

    Add CM specific configuration
    Change-Id: Id442974791a6f1009b093546806598c4d0edc887

---
+++ cm.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+TARGET_VENDOR := moto
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
