ab78309f13f31a2177ba1de9aa1b837e2d28cef1
dhacker29 <dhackerdvm@gmail.com>
2014-12-23 00:01:30 -0500

    Shamu: Add ffmpeg codecs
    Change-Id: I810a22c73eaa94ddee0c34c6262f2d7bce44929a

--- device.mk
+++ device.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
