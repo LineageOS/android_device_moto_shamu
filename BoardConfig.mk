34cd288210b8b646b19cc7adc79e0edb2eb97a87
Scott Mertz <scott@cyngn.com>
2015-01-25 10:44:43 -0800

    shamu: only support f2fs on linux builds
    Change-Id: Ifb9b53100f89bdc7b972532294d75a42e27fdc09

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,8 +102,11 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 TARGET_RECOVERY_FSTAB = device/moto/shamu/fstab.shamu
+
 # Ensure f2fstools are built
+ifeq ($(HOST_OS),linux)
 TARGET_USERIMAGES_USE_F2FS := true
+endif
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/moto/shamu
 
