54798887d16aa65895ff18779baec9266e8bece3
Bill Yi <byi@google.com>
2015-02-09 10:35:00 -0800

    Add Contact adapter app
    BUG:19045823
    Change-Id: I64f5b32649fc0022114d8352230150544969d5c5

--- self-extractors/root/device-vendor.mk
+++ self-extractors/root/device-vendor.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_PACKAGES += \
+    BuaContactAdapter
+
 LOCAL_STEM := shamu/device-partial.mk
 
 $(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
