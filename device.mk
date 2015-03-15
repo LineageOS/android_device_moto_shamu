d53c9ebad5ad42c948affdda8208ef4e33f09e07
dhacker29 <dhackerdvm@gmail.com>
2015-03-15 03:23:08 -0400

    build.prop: Add persist.radio.sib16_support
    Change-Id: I46667a20e0420938ba55bc40664ebd9334f5d7c2

--- device.mk
+++ device.mk
@@ -222,7 +222,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.no_wait_for_card=1
+    persist.radio.no_wait_for_card=1 \
+    persist.radio.sib16_support=1
 
 #Reduce IMS logging
 PRODUCT_PROPERTY_OVERRIDES += \
