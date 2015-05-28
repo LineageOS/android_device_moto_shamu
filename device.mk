b2b4b55f663e41352096fdb2525d663c3444638e
dhacker29 <dhackerdvm@gmail.com>
2015-05-28 00:13:14 -0400

    Set WiFi calling props
    Change-Id: I8db045bbc80fba70950b665640761bb4a2d736cb

--- device.mk
+++ device.mk
@@ -225,6 +225,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.no_wait_for_card=1 \
     persist.radio.sib16_support=1
 
+# WiFi calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
 # Rich Communications Service is disabled in 5.1
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rcs.supported=0
