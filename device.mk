3e26e6858456f2597317e97f2cee0d5434f51aad
Steve Kondik <steve@cyngn.com>
2015-06-19 20:09:35 +0300

    shamu: don't need to copy FFMPEG config anymore
    Change-Id: Icce25916bfc0bd1dedf9858196ff4b81f61cbe0c

--- device.mk
+++ device.mk
@@ -48,7 +48,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
