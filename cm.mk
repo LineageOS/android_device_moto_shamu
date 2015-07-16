1c81e328fdf83e8cae554a73dab4c64919b94ec1
Zyg0te <edvard.holst@gmail.com>
2015-07-16 11:02:32 +0200

    Corrected the boot animation screen dimention parameters
    -TARGET_SCREEN_HEIGHT and TARGET_SCREEN_WIDTH was seemingly flipped.
    
    Change-Id: I176c837362fcd4e5d6bb62211c30f4e2f034f48d

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1440
-TARGET_SCREEN_WIDTH := 2560
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
