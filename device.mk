6c2c0dafb01dfd2caef1e9033d6bd2164a79ea71
Alex Cruz <mazdarider23@gmail.com>
2015-10-08 16:36:30 +0000

    shamu: remove verity check on boot
    Change-Id: I72b3998c3d28791f4422eb4a5c7cba089f9d0865

--- device.mk
+++ device.mk
@@ -372,10 +372,6 @@ $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/msm_sdcc.1/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
 
-PRODUCT_PACKAGES += \
-    slideshow \
-    verity_warning_images
-
 # setup scheduler tunable
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.qualcomm.perf.cores_online=2
