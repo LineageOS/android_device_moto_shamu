845f8d2fab1627e92b7861d5ae12d65106aa7c89
Dave Daynard <nardholio@gmail.com>
2015-10-16 11:39:59 +0300

    shamu: Don't buid recovery_from_boot_patch
    Change-Id: I221bef73913aab768750329ef7306eb22a43d7a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M
 
+BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 # Shader cache config options
