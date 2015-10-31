10adce0299ab86067b215d1dcc057c35d53c5a15
Roman Birg <roman@cyngn.com>
2015-10-30 17:00:27 -0700

    Revert "IMSEnabler: Application to toggle the Enhanced 4G LTE Mode setting"
    This reverts commit b7c44976af5449a93b76718d86a512a821548ce4.
    
    Change-Id: Id0c768a3343cad6ab53630dca0c9ca9e50518de5

--- device.mk
+++ device.mk
@@ -233,10 +233,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # never dexopt the MotoSignature
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
-# Include IMSEnabler
-PRODUCT_PACKAGES += \
-    IMSEnabler
-
 # WiFi calling
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan.enable=true \
