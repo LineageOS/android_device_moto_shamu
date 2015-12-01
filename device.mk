de39c47fc2bfd08435e78b3a8a5ec307c305e3ea
beanstown106 <nbenis106@gmail.com>
2015-12-01 12:07:54 -0500

    shamu: add facelock and drm props
    Change-Id: I2c3cd427b0a9e51beea9e7982e1b710cc54c557d

--- device.mk
+++ device.mk
@@ -162,7 +162,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false \
-    ro.audio.monitorRotation=true
+    ro.audio.monitorRotation=true \
+    drm.service.enabled=true \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
 
 # Audio effects
 PRODUCT_PACKAGES += \
