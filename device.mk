5612bc7c065b686c9651633a91cf77924337ffbe
Martijn Coenen <maco@google.com>
2016-02-22 10:40:02 +0100

    Shamu: NFC: Enable HCE-Felica support.
    Bug: 22379408
    Change-Id: Ie793226a156e24ed0cd84f7cfbe5d664c776c223

--- device.mk
+++ device.mk
@@ -294,6 +294,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
     device/moto/shamu/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/moto/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
 
