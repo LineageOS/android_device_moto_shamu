25540c9bb1fbb03917e599913feaa5dde74d6bca
Elektroschmock <elektroschmock78@googlemail.com>
2016-04-13 21:14:42 +0200

    shamu: fix stray backslash
    Change-Id: I001dd5592aaf5df4a2684fa8931b932532d0ccee

--- device.mk
+++ device.mk
@@ -181,7 +181,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     fmas.spkr_6ch=35,20,110 \
     fmas.spkr_2ch=35,25 \
     fmas.spkr_angles=10 \
-    fmas.spkr_sgain=0 \
+    fmas.spkr_sgain=0
 
 PRODUCT_PACKAGES += \
     libqomx_core \
