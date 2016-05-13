d0ec18b0da0c7c5868c2b44f6290a8e3e47ef236
Elektroschmock <elektroschmock78@googlemail.com>
2016-05-13 20:29:23 +0200

    shamu: Update fingerprint to MOB30I
    Change-Id: Ic41e205c551396b6a91d342ae0d8672c909d2025

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := moto
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30D/2704746:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30D 2704746 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0.1/MOB30I/2756745:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30I 2756745 release-keys"
