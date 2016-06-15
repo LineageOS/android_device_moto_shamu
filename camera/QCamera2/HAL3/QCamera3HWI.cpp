7d262bb7a8fe4c5a5153de7191a715bd36326ffd
Shuzhen Wang <shuzhenwang@google.com>
2016-06-15 10:43:58 -0700

    QCamera2: HAL3: Log BUFFER_ERROR as warning
    Bug: 28427118
    Change-Id: I4d6ef39cec846145bcaeee4feea9379a75b483b8

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -1789,14 +1789,14 @@ void QCamera3HardwareInterface::handleMetadataWithLock(
                 for (uint32_t k = 0; k < cam_frame_drop.cam_stream_ID.num_streams; k++) {
                    if (streamID == cam_frame_drop.cam_stream_ID.streamID[k]) {
                        // Send Error notify to frameworks with CAMERA3_MSG_ERROR_BUFFER
-                       CDBG("%s: Start of reporting error frame#=%d, streamID=%d",
+                       ALOGW("%s: Start of reporting error frame#=%d, streamID=%d",
                               __func__, i->frame_number, streamID);
                        notify_msg.type = CAMERA3_MSG_ERROR;
                        notify_msg.message.error.frame_number = i->frame_number;
                        notify_msg.message.error.error_code = CAMERA3_MSG_ERROR_BUFFER ;
                        notify_msg.message.error.error_stream = j->stream;
                        mCallbackOps->notify(mCallbackOps, &notify_msg);
-                       CDBG("%s: End of reporting error frame#=%d, streamID=%d",
+                       ALOGW("%s: End of reporting error frame#=%d, streamID=%d",
                               __func__, i->frame_number, streamID);
                        PendingFrameDropInfo PendingFrameDrop;
                        PendingFrameDrop.frame_number=i->frame_number;
