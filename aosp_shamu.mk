1723870aecaa2cec77aa7e13ec583f68b83ad0be
razorloves <razorloves@gmail.com>
2016-08-10 12:42:37 -0700

    shamu: correct naming of blob makefile in aosp_shamu.mk
    The new generic extract utils creates makefile with actual device name
    
    Change-Id: I179f91a56d8bac2c0edc0ab5e7be13e07c50da64

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -31,7 +31,7 @@ PRODUCT_MANUFACTURER := motorola
 #PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
-$(call inherit-product-if-exists, vendor/motorola/shamu/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
 
 PRODUCT_NAME := aosp_shamu
 
