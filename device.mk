4f2c4de18c016eb13b23f7bbfd5a0bde11ac021e
Elektroschmock <elektroschmock78@googlemail.com>
2016-08-17 20:03:00 +0200

    shamu: Stop building generic charger images
    Change-Id: Ib91c43d7e3741d7f26ef645caaf8d03365f56b7c

--- device.mk
+++ device.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     e2fsck
 
-# for off charging mode
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # for launcher layout
 #PRODUCT_PACKAGES += \
 #    ShamuLayout
