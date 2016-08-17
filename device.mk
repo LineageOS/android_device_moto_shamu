875c8596e8704bb53ee3b855142838c7e82a0c78
Elektroschmock <elektroschmock78@googlemail.com>
2016-08-17 20:17:12 +0200

    shamu: Cleanup device.mk
    delete items already copied by build
    
    Change-Id: Ieb40eeba6c718e599f267e3094bb9c8ebfe37d5f

--- device.mk
+++ device.mk
@@ -128,11 +128,6 @@ PRODUCT_PACKAGES += atmel.fw.apq8084
 PRODUCT_PACKAGES += \
     qmi_motext_hook
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    LiveWallpapersPicker \
-    librs_jni
-
 PRODUCT_PACKAGES += \
     gralloc.msm8084 \
     hwcomposer.msm8084 \
@@ -197,10 +192,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     lights.shamu
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    e2fsck
 
 # for launcher layout
 #PRODUCT_PACKAGES += \
