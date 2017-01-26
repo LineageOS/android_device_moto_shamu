4558748fe283700f7d65238aed22af6310f2cf34
Abhisek Devkota <ciwrl@lineageos.org>
2017-01-26 13:07:25 -0800

    Shamu: Verity on user builds only
    Change-Id: I17c2326086afe5f3946fa3b6e4742a4618d2b74a

--- device.mk
+++ device.mk
@@ -376,9 +376,12 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui
 $(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
 
+# only include verity on user builds for LineageOS
+ifeq ($(TARGET_BUILD_VARIANT),user)
 # setup dm-verity configs.
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/msm_sdcc.1/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
+endif
 
 # setup scheduler tunable
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
