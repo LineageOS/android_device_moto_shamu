7048f6389bcc63ce2070e494132448d6e8b924ab
beanstown106 <nbenis106@gmail.com>
2017-02-12 12:25:50 -0600

    shamu: update facelock properties for N
    Change-Id: Ib3768c13deedfc3d3ea73983f5da53ca8e255303

--- device.mk
+++ device.mk
@@ -155,12 +155,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Face lock
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.facelock.black_timeout=400 \
-    ro.facelock.det_timeout=1500 \
-    ro.facelock.rec_timeout=2500 \
-    ro.facelock.lively_timeout=2500 \
-    ro.facelock.est_max_time=600 \
-    ro.facelock.use_intro_anim=false
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=2500 \
+    ro.facelock.rec_timeout=3500 \
+    ro.facelock.est_max_time=600
 
 # Audio effects
 PRODUCT_PACKAGES += \
