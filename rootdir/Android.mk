0e48b1e855b1f94bf056eab9bc3fe53a3525c3a5
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-15 10:41:18 -0700

    Move uevent.qcom.rc to /vendor
    Change-Id: I7e42c803561606128314bc4fb0f51fd02270d9fd

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -51,9 +51,10 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ueventd.shamu.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/ueventd.shamu.rc
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
 
 include $(BUILD_PREBUILT)
