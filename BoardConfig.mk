ffa5aa804b4dea08c749813cea7a4476023821e0
Christian Oder <myself5@carbonrom.org>
2017-09-02 22:04:56 +0200

    shamu: add vnd_shamu.txt
    file originates from hardware/broadcom/libbt/include/vnd_shamu.txt, head 8c0bd6106978c165b249de54188b19868dfc2ff8
    Move, as we intend to edit it.
    
    Change-Id: I3704c2864baa2b1a71f3dd3bfbda88dff28b8df8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu/bluetooth_extra
 else
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/shamu/bluetooth
 endif
+BOARD_CUSTOM_BT_CONFIG := device/moto/shamu/bluetooth/vnd_shamu.txt
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := msm8084
