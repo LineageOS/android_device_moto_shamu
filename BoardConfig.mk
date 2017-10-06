c72cf82cad21c2bd5e9809df1012c5b1f1d7dc9c
elektroschmock <elektroschmock78@googlemail.com>
2017-10-06 13:58:56 +0200

    shamu: Add health_board_shamu.cpp to get rid of logspam
    * Don't use lineage charger
    
    Change-Id: I148d9688af5e3b5fb70b78ab845394178375b276

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,8 +100,11 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+# Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+WITH_LINEAGE_CHARGER := false
 
 TARGET_RECOVERY_FSTAB = device/moto/shamu/fstab.shamu
 # Ensure f2fstools are built
@@ -125,7 +128,7 @@ BOARD_HAS_AUDIO_DSP := true
 
 USE_DEVICE_SPECIFIC_CAMERA:= true
 
-BOARD_HAL_STATIC_LIBRARIES := libdumpstate.shamu
+BOARD_HAL_STATIC_LIBRARIES += libdumpstate.shamu
 
 USE_CLANG_PLATFORM_BUILD := true
 
