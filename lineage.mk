964510c7601fcaf1d34779a5b6733138ac787381
elektroschmock <elektroschmock78@googlemail.com>
2017-12-07 12:05:18 +0100

    shamu: Cleanup the tree
    AOSP has abandoned us
    
    * Delete unused CleanSpec.mk
    * Delete unused aosp_shamu.mk and move config to lineage.mk
    * Delete unused AndroidProducts.mk
    
    Change-Id: I5f43fb1ec69e8586f1eeecf278eee3311553208a

--- lineage.mk
+++ lineage.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
@@ -5,15 +20,21 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/moto/shamu/device.mk)
+$(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-lineage
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_shamu
+PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
 
 TARGET_VENDOR := moto
 
