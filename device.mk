90b804ba0197a8294c69123638ce08d479f969cf
elektroschmock <elektroschmock78@googlemail.com>
2017-12-12 13:16:06 +0100

    Revert "Shamu: Use libfmas for audio effects"
    This reverts commit cd3d20605bd7dc9d752e3caf8ba719ee3483a54b.
    
    Change-Id: I1d95961f0d6b73f8f5bc59afe6f20b37c6f68aee

--- device.mk
+++ device.mk
@@ -181,12 +181,6 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcomvoiceprocessingdescriptors
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    fmas.spkr_6ch=35,20,110 \
-    fmas.spkr_2ch=35,25 \
-    fmas.spkr_angles=10 \
-    fmas.spkr_sgain=0
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
