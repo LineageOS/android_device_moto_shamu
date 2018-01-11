fa3734ba14ac7fc7091781fb4b2d37a54879d639
elektroschmock <elektroschmock78@googlemail.com>
2018-01-11 10:18:39 +0100

    shamu: Remove unused lineagehw
    Change-Id: Iafa34d9451f74f815ca2e5e44973d91fac788969

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,9 +137,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 USE_OPENGL_RENDERER := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
-# Lineage Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/lineagehw
-
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
 
