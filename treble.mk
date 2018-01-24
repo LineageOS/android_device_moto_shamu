933c0f530181ea46ab4707d184e885dfe6e21738
elektroschmock <elektroschmock78@googlemail.com>
2018-01-24 13:18:12 +0100

    shamu: Add dumpstate service.shamu to treble.mk
    Change-Id: I2af30bcff9fdf6f43ae9dccf65024575789865bc
    (cherry picked from commit 0470ad2121fd9a7b22f38c8728276cbd3155ec52)

--- treble.mk
+++ treble.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
+# Dumpstate
+PRODUCT_PACKAGES += \
+    android.hardware.dumpstate@1.0-service.shamu
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
