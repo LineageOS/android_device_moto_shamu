f1cb787b10510a85acffd3bf16044928f691f2c4
elektroschmock <elektroschmock78@googlemail.com>
2018-01-25 10:41:16 +0100

    shamu: Remove non-existing build targets
    Change-Id: If6f5f5631c1a6912010df216b83b4d66284db24d

--- device.mk
+++ device.mk
@@ -345,9 +345,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # setup HWUI configs
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
-$(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
-$(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
-
 # only include verity on user builds for LineageOS
 ifeq ($(TARGET_BUILD_VARIANT),user)
 # setup dm-verity configs.
