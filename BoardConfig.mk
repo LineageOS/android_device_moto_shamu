bef58644c2be3cad88d754a8bd827d95f7505a18
elektroschmock <elektroschmock78@googlemail.com>
2018-01-29 08:08:31 +0100

    shamu: Move props into seperate system.prop file
     * Move properties for default.prop to one place
    
    Change-Id: Id91484876eab0759378d4b1eae865cc5538e108e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The Android Open-Source Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -80,6 +81,9 @@ TARGET_BOOTLOADER_BOARD_NAME := shamu
 TARGET_NO_RPC := true
 
 TARGET_BOARD_INFO_FILE := device/moto/shamu/board-info.txt
+
+# Properties
+TARGET_SYSTEM_PROP += device/moto/shamu/system.prop
 
 TARGET_USES_ION := true
 TARGET_HW_DISK_ENCRYPTION := false
