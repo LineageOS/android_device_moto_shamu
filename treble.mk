145b1d098009be5c1cd89efb75b529b4d1befe74
elektroschmock <elektroschmock78@googlemail.com>
2018-03-05 07:58:53 +0100

    shamu: Add health HAL to treble.mk and manifest.xml
    Change-Id: Ib284db6fef6d0e7faec3206379eb9e4cf775aaad

--- treble.mk
+++ treble.mk
@@ -50,6 +50,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
+# Health
+PRODUCT_PACKAGES += \
+   android.hardware.health@1.0-convert \
+   android.hardware.health@1.0-impl \
+   android.hardware.health@1.0-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
