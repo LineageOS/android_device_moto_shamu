9f35de72e8347ba15040621e953ebb2dec1c9e34
Elektroschmock <elektroschmock78@googlemail.com>
2018-04-25 19:02:46 +0200

    shamu: power: Set TARGET_HAS_LEGACY_POWER_STATS := true
    Change-Id: I146946d9da9c955ddebb1adc27849c179847b662

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.shamu
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Power
+TARGET_HAS_LEGACY_POWER_STATS := true
 TARGET_HAS_NO_WIFI_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/1-004a/tsp"
