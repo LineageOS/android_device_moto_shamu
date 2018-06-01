1b55e0ef84a8e432cc2833c92c467a54e6ab2297
Bruno Martins <bgcngm@gmail.com>
2018-06-01 20:53:33 +0200

    shamu: Include DRM service
    Change-Id: I76eccf8e0741fb7f756508f1f9cffafc3c8b7891
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- treble.mk
+++ treble.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Dumpstate
 PRODUCT_PACKAGES += \
