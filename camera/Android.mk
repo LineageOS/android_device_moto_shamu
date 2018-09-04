f183b795cd96b954b899d81290daba1c475bc4ed
elektroschmock <elektroschmock78@googlemail.com>
2018-09-04 13:56:16 +0200

    shamu: camera: Remove obsolete BUILD_TINY_ANDROID check
    Change-Id: Ic0512b9eb0c0f412b5b8f5a56e62f61492f89ecf

--- camera/Android.mk
+++ camera/Android.mk
@@ -14,9 +14,7 @@ MM_V4L2_DRIVER_LIST += msm8084
 
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(USE_CAMERA_STUB),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
+    include $(call all-subdir-makefiles)
   endif
 endif
 
