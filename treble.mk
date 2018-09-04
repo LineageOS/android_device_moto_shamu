8a653dea2d807f1afb5ad9079c86c79dd9850082
Bruno Martins <bgcngm@gmail.com>
2018-09-04 04:43:23 +0000

    shamu: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- treble.mk
+++ treble.mk
@@ -85,6 +85,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.1-service.shamu
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.shamu
