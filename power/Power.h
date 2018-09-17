83c8b0d35398f0ac26484b32c696a48b143d1a9c
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-17 12:14:16 -0700

    shamu: power: Return empty subsystems power stats and always use Power@1.1
    Instead of supporting a 1.0 HAL, always build the 1.1 HAL and just return an empty
    subsystems supported count for devices that do not have wifi power stats
    
    Change-Id: I9b4e37fb657b07529136bf7880de0f9fe5115a1c

--- power/Power.h
+++ power/Power.h
@@ -18,11 +18,7 @@
 #ifndef ANDROID_HARDWARE_POWER_V1_1_POWER_H
 #define ANDROID_HARDWARE_POWER_V1_1_POWER_H
 
-#ifdef V1_0_HAL
-#include <android/hardware/power/1.0/IPower.h>
-#else
 #include <android/hardware/power/1.1/IPower.h>
-#endif
 #include <vendor/lineage/power/1.0/ILineagePower.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
@@ -31,20 +27,12 @@
 namespace android {
 namespace hardware {
 namespace power {
-#ifdef V1_0_HAL
-namespace V1_0 {
-#else
 namespace V1_1 {
-#endif
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
 using ::android::hardware::power::V1_0::PowerHint;
-#ifdef V1_0_HAL
-using ::android::hardware::power::V1_0::IPower;
-#else
 using ::android::hardware::power::V1_1::IPower;
-#endif
 using ::vendor::lineage::power::V1_0::ILineagePower;
 using ::vendor::lineage::power::V1_0::LineageFeature;
 using ::android::hardware::Return;
@@ -61,11 +49,9 @@ struct Power : public IPower, public ILineagePower {
     Return<void> setFeature(Feature feature, bool activate) override;
     Return<void> getPlatformLowPowerStats(getPlatformLowPowerStats_cb _hidl_cb) override;
 
-#ifndef V1_0_HAL
     // Methods from ::android::hardware::power::V1_1::IPower follow.
     Return<void> getSubsystemLowPowerStats(getSubsystemLowPowerStats_cb _hidl_cb) override;
     Return<void> powerHintAsync(PowerHint hint, int32_t data) override;
-#endif
 
     // Methods from ::vendor::lineage::power::V1_0::ILineagePower follow.
     Return<int32_t> getFeature(LineageFeature feature) override;
