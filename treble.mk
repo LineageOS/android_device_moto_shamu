f2c9a34cf65929d7933d3e90629a70a1c19bacfd
Elektroschmock <elektroschmock78@googlemail.com>
2018-10-04 18:27:48 +0200

    shamu: Switch to power HAL 1.1
    Change-Id: Iefae0431fe6a0703b958a9c54f056d93ca07e9ad

--- treble.mk
+++ treble.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.shamu
+    android.hardware.power@1.1-service.shamu
 
 # RenderScript
 PRODUCT_PACKAGES += \
