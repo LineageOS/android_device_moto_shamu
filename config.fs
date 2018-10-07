e32334df311c8c8f72e2b7e1d4eab62ad1143c53
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    shamu: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Furthermore, with System As Root / is a read-only filesystem
       so we can't create our mountpoints from init script anymore
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b
    Signed-off-by: elektroschmock <elektroschmock78@googlemail.com>

--- config.fs
+++ config.fs
@@ -9,3 +9,28 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[fsg/]
+mode: 0755
+user: AID_ROOT
+group: AID_ROOT
+caps: 0
+
+[oem/]
+mode: 0550
+user: AID_ROOT
+group: AID_ROOT
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
