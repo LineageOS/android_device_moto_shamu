c985a46d36d650cadc8f3ec2585eed67d83d2e21
Nolen Johnson <johnsonnolen@gmail.com>
2018-12-11 04:09:47 -0500

    shamu: Use $(LOCAL_PATH) where possible
    * LOCAL_PATH isn't set in BoardConfig, so leave it as is.
    * QCamera* doesn't like this at all, so leave it as is.
    * Replace all hard-codings of device/moto/shamu with $(LOCAL_PATH).
    
    Change-Id: I0303905a1a19990bca2d78655b3e4cd12453f977

--- lineage_shamu.mk
+++ lineage_shamu.mk
@@ -25,7 +25,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/moto/shamu/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit proprietary files
 $(call inherit-product, vendor/motorola/shamu/shamu-vendor.mk)
