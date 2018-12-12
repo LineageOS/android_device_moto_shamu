f8b791393405c4ccf445835326b12acd3f025714
Elektroschmock <elektroschmock78@googlemail.com>
2018-12-12 17:26:18 +0100

    shamu: Remove dead build target
    * power.shamu is dead since we moved to the new binderized power HAL.
    
    Change-Id: I1614dae5a55c5318b6ed2439350fcc2e18dd1105

--- device.mk
+++ device.mk
@@ -176,10 +176,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    power.shamu
-
 # Ramdisk
  PRODUCT_PACKAGES += \
     fstab.shamu \
