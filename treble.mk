5405aa817ed26b7c713025b3a4928e7b6ad804d2
elektroschmock <elektroschmock78@googlemail.com>
2018-12-19 10:12:59 +0100

    shamu: Move keymaster to hwbinder
    Change-Id: I20a4a81711285d2bea5906cd24cc392ca9ab43c2

--- treble.mk
+++ treble.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Light
 PRODUCT_PACKAGES += \
