c4825a2c7e9eb2e101b3f72f40abeac892cd4560
elektroschmock <elektroschmock78@googlemail.com>
2018-12-19 13:53:21 +0100

    shamu: Move memtrack to hwbinder
    Change-Id: I30c246fa3e4d85fc1cb784647c1cc51b2dfdeed3

--- treble.mk
+++ treble.mk
@@ -47,7 +47,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \
