6451e4cd16894e662a79a74c85830338f04f2d65
Joey <joey@lineageos.org>
2019-01-09 18:02:45 +0000

    shamu: Build Trust HAL
    Change-Id: I2a426b2c2dda70f41e3c3ea6f85905b0daf65f0d
    Signed-off-by: Joey <joey@lineageos.org>

--- hidl.mk
+++ hidl.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.1-service.shamu
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
