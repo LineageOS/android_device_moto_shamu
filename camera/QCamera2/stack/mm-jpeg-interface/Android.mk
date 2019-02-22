be0d296c575796c13794f03b517ec9760906d5ff
elektroschmock <elektroschmock78@googlemail.com>
2019-02-22 10:00:52 +0100

    shamu: camera: Cleanup makefiles
    Change-Id: I2c2d6009e7239b637fd95dd9cae4f76acebec97e

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -5,7 +5,11 @@ include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_32_BIT_ONLY := true
-LOCAL_CFLAGS+= -D_ANDROID_
+
+LOCAL_CFLAGS += \
+    -D_ANDROID_ \
+    -DUSE_ION \
+    -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=2
 
 LOCAL_C_INCLUDES+= $(kernel_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
@@ -18,20 +22,6 @@ LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/../../../mm-image-codec/qexif \
     $(LOCAL_PATH)/../../../mm-image-codec/qomx_core
 
-ifeq ($(strip $(TARGET_USES_ION)),true)
-    LOCAL_CFLAGS += -DUSE_ION
-endif
-
-ifneq (,$(filter  msm8610,$(TARGET_BOARD_PLATFORM)))
-    LOCAL_CFLAGS+= -DLOAD_ADSP_RPC_LIB
-endif
-
-ifneq (,$(filter  msm8974,$(TARGET_BOARD_PLATFORM)))
-    LOCAL_CFLAGS+= -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=2
-else
-    LOCAL_CFLAGS+= -DMM_JPEG_CONCURRENT_SESSIONS_COUNT=1
-endif
-
 LOCAL_SRC_FILES := \
     src/mm_jpeg_queue.c \
     src/mm_jpeg_exif.c \
@@ -41,8 +31,14 @@ LOCAL_SRC_FILES := \
     src/mm_jpegdec_interface.c \
     src/mm_jpegdec.c
 
-LOCAL_MODULE           := libmmjpeg_interface
-LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core
+LOCAL_MODULE := libmmjpeg_interface
+
+LOCAL_SHARED_LIBRARIES := \
+    libdl \
+    libcutils \
+    liblog \
+    libqomx_core
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := true
