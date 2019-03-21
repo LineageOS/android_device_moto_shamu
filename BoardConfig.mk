cd9f6707d460c1a08cf8becfae19f39e88c32884
Nolen Johnson <johnsonnolen@gmail.com>
2019-03-21 05:49:04 +0000

    shamu: Don't DexPreOpt Prebuilts
    * This lets full OpenGApps fit fine when built in, and has little effect
      on first boot (most our apps are source built).
    
    Change-Id: Id56708240f18fcd082a43561a0bba1da1f974d67

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      DONT_DEXPREOPT_PREBUILTS := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
     endif
