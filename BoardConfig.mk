a95e73dbf2063e4155af25eafee6b6f65b846d9b
Elektroschmock <elektroschmock78@googlemail.com>
2019-09-23 19:56:19 +0200

    shamu: Remove unused compatibility_matrix.xml
    Change-Id: I0bfc3b00e9a9b0a2c88a43b31e6d653aca391530

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,6 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 # Manifests
 DEVICE_FRAMEWORK_MANIFEST_FILE += system/libhidl/vintfdata/manifest_healthd_exclude.xml
 DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
-DEVICE_MATRIX_FILE := device/moto/shamu/compatibility_matrix.xml
 
 # Power
 TARGET_HAS_LEGACY_POWER_STATS := true
