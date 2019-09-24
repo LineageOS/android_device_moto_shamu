f45e9a970ca6acd024b7e01425758cbfe96ced48
Elektroschmock <elektroschmock78@googlemail.com>
2019-09-24 21:43:55 +0200

    Revert "shamu: Add p2p_no_group_iface=1 to p2p_supplicant_overlay"
    This reverts commit c824b02b897993304fc6f3c1680fc642825774ef.
    
    Change-Id: If4584fef23e68f4d7bf7b339120fba0b7f4b44c6

--- device.mk
+++ device.mk
@@ -251,7 +251,6 @@ PRODUCT_COPY_FILES += \
 
 # WiFi config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # WiFi firmware
