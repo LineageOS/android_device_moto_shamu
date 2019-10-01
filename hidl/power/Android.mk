dc2cfcc7115187d052eb7852c77a14db35a68659
Steven Moreland <smoreland@google.com>
2019-10-01 11:01:16 -0700

    shamu: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: I640a3f0e0839214523627bf0b59fd5ae9dd2c61c

--- hidl/power/Android.mk
+++ hidl/power/Android.mk
@@ -38,8 +38,6 @@ LOCAL_SHARED_LIBRARIES := \
     libdl \
     libbase \
     libhidlbase \
-    libhidltransport \
-    libhwbinder \
     libutils \
     android.hardware.power@1.2 \
     vendor.lineage.power@1.0
