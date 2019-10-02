187976e58444b842af42c5605585cc633a62231c
elektroschmock <elektroschmock78@googlemail.com>
2019-10-02 15:22:43 +0200

    shamu: Convert ConfigStore properties to system properties
    Configstore is deprecated since Android 10 due to high memory consumption
    
    Reference:
    https://source.android.com/devices/architecture/configuration
    
    Change-Id: I39ed9422eb95a11153593a7c7b0739e3a994667c

--- system_prop.mk
+++ system_prop.mk
@@ -100,6 +100,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.qcril_uim_vcc_feature=1 \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so
 
+# Surfaceflinger
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=2048
+
 # Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
