4211ce0d4852040a4faa14465179baf201ea591f
Elektroschmock <elektroschmock78@googlemail.com>
2019-10-27 13:46:54 +0100

    shamu: Build Clearkey 1.2 plugin and update vintf
    Also remove the widevine plugin we don't have
    
    Reference:
    https://github.com/LineageOS/android_hardware_interfaces/blob/lineage-17.0/drm/1.1/README.md
    
    Change-Id: Ie4ea8d79496ba6181ef6c45b0ce0a993a3e442c4

--- hidl.mk
+++ hidl.mk
@@ -31,7 +31,9 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
+
 
 # Dumpstate
 PRODUCT_PACKAGES += \
