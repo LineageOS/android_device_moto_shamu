f3dc12d06800b67a6973608450af001686c39993
chrmhoffmann <chrmhoffmann@gmail.com>
2020-02-02 12:53:51 +0100

    shamu: Drop external camera config
    Change-Id: If6e611bbb7897bfbcdba5e381c62348d8d91373c

--- device.mk
+++ device.mk
@@ -71,9 +71,6 @@ PRODUCT_PACKAGES += \
     camera.msm8084 \
     libmm-qcamera
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
