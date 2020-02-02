dcd1d68a4604cf00f55941911df75de1cff56a0d
chrmhoffmann <chrmhoffmann@gmail.com>
2020-02-02 12:53:51 +0100

    shamu: Drop external camera config
    Change-Id: If6e611bbb7897bfbcdba5e381c62348d8d91373c

--- device.mk
+++ device.mk
@@ -71,11 +71,36 @@ PRODUCT_PACKAGES += \
     camera.msm8084 \
     libmm-qcamera
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
+    copybit.msm8994 \
+    gralloc.msm8994 \
+    hwcomposer.msm8994 \
+    memtrack.msm8994 \
+    liboverlay \
+    libtinyxml
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    OneplusDoze
+>>>>>>> 02121f09... oneplus2: Drop external camera config
 
 # Dex-pre-opt exclusions
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
