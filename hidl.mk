ba96d4baed3a85d562c2842c965641efbbdaba7a
Elektroschmock <elektroschmock78@googlemail.com>
2020-04-06 16:20:01 +0200

    Revert "shamu: don't build thermal HAL for now"
    This reverts commit 9e67731d81c26a07e21e6952056746d517222e9b.
    
    Re-enable after I fixed my derp
    
    Change-Id: Id335d4297dbe05b00f303a37c5b4ce71a53c1efd

--- hidl.mk
+++ hidl.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017 The The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.1-service.shamu
 
 # Trust
 PRODUCT_PACKAGES += \
