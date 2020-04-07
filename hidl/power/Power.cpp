c05ca1f1fdaf63f0dcbf80adcbdfba614bae3ce7
Elektroschmock <elektroschmock78@googlemail.com>
2020-04-07 13:08:40 +0200

    shamu: Unbreak the double tap to wake gesture
    Change-Id: Ieec93bf7776adc506da0bf133356cfb4870db47d

--- hidl/power/Power.cpp
+++ hidl/power/Power.cpp
@@ -71,7 +71,7 @@ Return<void> Power::setFeature(Feature feature, bool activate) {
     switch (feature) {
 #ifdef TAP_TO_WAKE_NODE
         case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
-            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE, true);
+            ::android::base::WriteStringToFile(activate ? "AUTO" : "OFF", TAP_TO_WAKE_NODE, true);
             break;
 #endif
         default:
