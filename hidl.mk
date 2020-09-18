35d794ecfa345987db70aabe8bb6e801e1fbec09
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    shamu: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- hidl.mk
+++ hidl.mk
@@ -61,6 +61,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    android.hardware.health@2.0-service.shamu
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
