9be0c6f7942461cf6dcff1d5e7c4ceb63c834a59
Christian Hoffmann <chrmhoffmann@gmail.com>
2021-06-27 17:58:12 +0200

    shamu: Disable qcom AEC and NS effects.
    * They don't work and create echo.
    * Use AOSP instead - they get recognized by webrtc and disabled.
    
    Change-Id: I26570eb91597688c3c5257e7e1748971069f4ffd
    (cherry picked from commit 47e8346a31ef425dc0fa7b9be8f9377edc364eb4)
    (cherry picked from commit 4363dbe3811046469abd006127d16dfae70ec4e8)

--- device.mk
+++ device.mk
@@ -51,9 +51,7 @@ PRODUCT_COPY_FILES += \
 # Audio Effects
 PRODUCT_PACKAGES += \
     libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors
+    libqcomvisualizer
 
 # Bluetooth
 PRODUCT_PACKAGES += \
